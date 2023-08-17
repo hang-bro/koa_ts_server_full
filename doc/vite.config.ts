d9d121b829808b2ee49bdece91ca739e72940b15
huangyuhang <772269729@qq.com>
2023-08-17 09:43:42 +0800

    git

--- doc/vite.config.ts
+++ doc/vite.config.ts
@@ -4,8 +4,7 @@
  * @LastEditors: HYH
  * @LastEditTime: 2023-07-28 20:08:45
  */
-// 类型提示
-import path, { resolve } from 'path'
+import { resolve } from 'path'
 import { defineConfig, loadEnv, searchForWorkspaceRoot } from 'vite'
 import usePlugin from './vite/usePlugin'
 
@@ -31,6 +30,7 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
       // },
       host: true,
       port: env.VITE_FRONT_PORT,
+      /**可读取的文件夹 */
       fs: {
         allow: [searchForWorkspaceRoot(process.cwd()), '../app/enum'],
       },
@@ -46,13 +46,32 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
       //
     },
     build: {
-      outDir: path.join(__dirname, '../server/public/doc'),
+      // outDir: path.join(__dirname, '../server/public/doc'),
       emptyOutDir: true,
       rollupOptions: {
         output: {
           chunkFileNames: 'js/[name]-[hash].js', // 引入文件名的名称
           entryFileNames: 'js/[name]-[hash].js', // 包的入口文件名称
           assetFileNames: '[ext]/[name]-[hash].[ext]', // 资源文件像 字体，图片等
+          /**
+           * @description  清除文件名格式不正确问题
+           * @param name
+           * @returns
+           */
+          // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+          sanitizeFileName(name) {
+            // eslint-disable-next-line no-control-regex
+            const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g
+            const DRIVE_LETTER_REGEX = /^[a-z]:/i
+            const match = DRIVE_LETTER_REGEX.exec(name)
+            const driveLetter = match ? match[0] : ''
+            // A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
+            // Otherwise, avoid them because they can refer to NTFS alternate data streams.
+            let file = driveLetter + name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '')
+            if (file.startsWith('_')) file = file.slice(1) /**去除首位字符串为 "_" , fix github page访问不了的问题*/
+            return file
+          },
+
           // // 最小化拆分包
           // manualChunks(id) {
           //   if (id.includes('node_modules')) {
