49e83d2ab54986cb4b85626ec6ba863b51112404
huangyuhang <772269729@qq.com>
2023-08-17 15:25:21 +0800

    dsad

---
+++ build-github-page.ts
@@ -0,0 +1,20 @@
+/*
+ * @Description:
+ * @Author: HYH
+ * @LastEditors: HYH
+ * @LastEditTime: 2023-07-10 10:11:08
+ */
+
+import 'zx/globals'
+
+async function main() {
+  const distDir = path.join(__dirname, './github-page')
+  // const distZipDir = path.join(__dirname, './dist.zip')
+  // console.log(`正在删除dist文件夹`)
+  // if (fs.existsSync(distDir)) fs.rmSync(distDir, { recursive: true })
+  // if (fs.existsSync(distZipDir)) fs.rmSync(distZipDir, { recursive: true })
+  console.log(`distDir ==>`, distDir)
+  await $`cd ./docs && npx vite build --outDir="../github-page"`
+}
+
+main()
