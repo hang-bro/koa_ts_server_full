41de47d810b19bdee364d71982c260fbcc065506
huangyuhang <772269729@qq.com>
2023-08-17 15:39:13 +0800

    commit github-page

--- build-github-page.ts
+++ build-github-page.ts
@@ -16,10 +16,11 @@ async function main() {
   console.log(`distDir ==>`, distDir)
   // await $`cd ./docs && npx vite build --outDir="../github-page" --base="/koa_ts_server_full_doc"`
   await $`cd ./docs`
-  await $`cd ../`
-  await $`cd ./github-page`
+  await $`cd D:\\my-gitee\\koa_ts_server_full\\github-page`
+  // await $`cd ../`
+  // await $`cd ./github-page`
   await $`git add .`
-  await $`git commit -m"commit"`
+  await $`git commit -m"commit github-page"`
   await $`git push`
 }
 
