5056654e36e5f62f5df765037f795441240d1abc
huangyuhang <772269729@qq.com>
2023-08-18 08:58:04 +0800

    rename

--- build-github-page.ts
+++ build-github-page.ts
@@ -14,8 +14,8 @@ async function main() {
   // if (fs.existsSync(distDir)) fs.rmSync(distDir, { recursive: true })
   // if (fs.existsSync(distZipDir)) fs.rmSync(distZipDir, { recursive: true })
   console.log(`distDir ==>`, distDir)
-  // await $`cd ./docs && npx vite build --outDir="../github-page" --base="/koa_ts_server_full_doc"`
-  // await $`cd ./docs`
+  // await $`cd ./doc && npx vite build --outDir="../github-page" --base="/koa_ts_server_full_doc"`
+  // await $`cd ./doc`
   await $`cd D:\\my-gitee\\koa_ts_server_full\\github-page`
   // await $`cd ../`
   // await $`cd ./github-page`
