f1f0328a8093b37c9bad3af208ae17c9e86d7387
huangyuhang <772269729@qq.com>
2023-08-23 13:44:12 +0800

    patch

--- doc/src/views/book/index.vue
+++ doc/src/views/book/index.vue
@@ -2,14 +2,15 @@
   <main class="w-full h-full p-5">
     <h1>BOOK</h1>
     <section>
-      <el-select class=" !w-full" @change="getBook" v-model="selectTitle" filterable>
+      <el-select class="!w-full" @change="getBook" v-model="selectTitle" filterable>
         <el-option v-for="item in bookList" :key="item" :label="item" :value="item" />
       </el-select>
     </section>
-    <section class="h-[500px] overflow-auto bg-white" v-draggable ref="bookRef">
-      <div v-for="p in html">
-        <p class="m-1 text-gray-500">{{ p }}</p>
-      </div>
+    <section
+      class="h-[500px] overflow-auto no-scroll bg-[url(https://img0.baidu.com/it/u=367684367,2198835658&fm=253&fmt=auto&app=120&f=JPEG?w=281&h=500)] bg-cover"
+      v-draggable
+      ref="bookRef">
+      <div v-html="html" class="whitespace-pre-wrap text-gray-600 font-semibold leading-[35px]"></div>
     </section>
   </main>
 </template>
@@ -23,6 +24,7 @@ const bookList = ref([])
 const getBook = (name: string) => {
   http.get<any>(`${import.meta.env.VITE_BASE_URL}/download/book/${name}`, {}, { responseType: 'text' }).then((res) => {
     console.log(`res ==>`, res)
+    html.value = res
   })
 }
 onMounted(() => {
@@ -37,7 +39,8 @@ const scrollTop = () => (bookRef.value.scrollTop = 0)
 </script>
 <style lang="scss" scoped>
 section {
-  width: 500px;
+  width: 300px;
+  font-size: 15px;
   margin-bottom: 50px;
   border: 2px dashed #ccc;
   padding: 15px;
